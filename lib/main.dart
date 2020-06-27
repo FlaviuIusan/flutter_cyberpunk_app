02acf38982f14091b332f8dc921efa402d1f1164
Narcis <cbnarcis@yahoo.com>
2020-06-27 22:14:20 +0300

    commit two

--- lib/main.dart
+++ lib/main.dart
@@ -7,9 +7,6 @@ void main() {
 }
 
 class MainApp extends StatelessWidget {
-  //two
-  //modification
-  //1
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
