bab9b0656095cc3d19c46a859ebc1838ac19178b
Flavius890 <65131626+Flavius890@users.noreply.github.com>
2020-06-29 01:35:30 +0300

    main redefined

--- lib/main.dart
+++ lib/main.dart
@@ -8,102 +8,100 @@ import 'mainmenu.dart';
 void main() {
   runApp(MainApp());
 }
+
 AudioCache cache = new AudioCache();
 
 AudioPlayer audioPlayer;
 bool _buttonstate = true;
 
-
-
 class MainApp extends StatelessWidget {
-
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'projectx',
       home: MainScreen(),
-
     );
   }
 }
 
 class MainScreen extends StatefulWidget {
-
   @override
   _MainScreenState createState() => _MainScreenState();
 }
 
-
-
 class _MainScreenState extends State<MainScreen> {
-
   String _background = "intro.gif"; //imaginea in _mainBody -> decoration
   @override
   Widget build(BuildContext context) {
     SystemChrome.setEnabledSystemUIOverlays([SystemUiOverlay.bottom]);
     cache.play("audio/logosound.mp3");
     setAudio();
+    /*size 150
+    backgroundColor: Colors.transparent,
+    flexibleSpace: Image(
+    image: AssetImage("titleanimation.gif"),
+    fit: BoxFit.cover,
+
+     */
     return Scaffold(
-      appBar: PreferredSize(
-        preferredSize: Size.fromHeight(150.0),
-        child:AppBar(
-          backgroundColor: Colors.transparent,
-          flexibleSpace: Image(
-            image: AssetImage("titleanimation.gif"),
-            fit: BoxFit.cover,
-          ),
-        ),
-      ),
-      body: _mainBody(
-      ),
+      body: _mainBody(),
     );
   }
 
   static Future<void> setAudio() async {
     audioPlayer = await cache.loop("audio/maintheme.mp3");
-
   }
 
-
-  Widget _mainBody(){
-
+  Widget _mainBody() {
     return Container(
-      decoration: BoxDecoration(
-        image: DecorationImage(
-          image: AssetImage(_background), //imaginea schimbata de timer
-          fit: BoxFit.fill,
-        ),
-      ),
-      alignment: Alignment(0,0.5),
-      child: AnimatedOpacity(
-
-          opacity: _buttonstate? 1.0:0.0,
-          duration: Duration(milliseconds: 2000),
-
-        child:GestureDetector(
-          child: Container(
-              width:200,
-              height: 50,
+      alignment: Alignment.center,
+      child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Container(
+              height: (MediaQuery.of(context).size.height / 3) * 1,
               decoration: BoxDecoration(
-                color: Colors.transparent,
                 image: DecorationImage(
-                    image:AssetImage("enterbutton.gif"),
-                    fit:BoxFit.fill
+                  image: AssetImage(
+                      "titleanimation.gif"), //imaginea schimbata de timer
+                  fit: BoxFit.fill,
                 ),
-              )
-          ),onTap:(){
-
-            //audioPlayer.stop();
-            Navigator.push(
-            context,
-            MaterialPageRoute(builder: (context) => MainMenuWidget()),
-        );
-      },
-      )
-      )
-
-    );}
-
+              ),
+            ),
+            Container(
+              height: (MediaQuery.of(context).size.height / 3) * 2,
+              width: (MediaQuery.of(context).size.width),
+              decoration: BoxDecoration(
+                image: DecorationImage(
+                  image: AssetImage(_background), //imaginea schimbata de timer
+                  fit: BoxFit.fill,
+                ),
+              ),
+              child: Align(
+              child: Container(
+                width: 200,
+                height: 50,
+                child: GestureDetector(
+                  child: Container(
+                      decoration: BoxDecoration(
+                    color: Colors.transparent,
+                    image: DecorationImage(
+                      image: AssetImage("enterbutton.gif"),
+                    ),
+                  )),
+                  onTap: () {
+                    //audioPlayer.stop();
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(builder: (context) => MainMenuWidget()),
+                    );
+                  },
+                ),
+              ),
+              ),
+            ),
+          ]),
+    );
+  }
 }
-
