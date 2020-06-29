aada7c88df0f9b534bdfbc70a677231aa56f6903
Narcis <cbnarcis@yahoo.com>
2020-06-29 07:34:07 +0300

    design update info

--- lib/wallpapers.dart
+++ lib/wallpapers.dart
@@ -41,10 +41,8 @@ class _WallpapersWidgetState extends State<WallpapersWidget> {
   Widget _buildImages() {
 
     return ListView.builder(
-        itemCount: 15,
+        itemCount: 11,
         itemBuilder: (BuildContext _context, int i) {
-            images.add("https://r1.ilikewallpaper.net/iphone-wallpapers/download/94641/cyberpunk-2077-game-2020-4k-iphone-wallpaper-ilikewallpaper_com.jpg");
-            images.add("https://assets.vg247.com/current//2020/03/cyberpunk_wallpaper_4.jpg");
             images.add("https://wallpapershome.com/images/pages/pic_h/19064.jpg");
             images.add("https://wallpapershome.com/images/pages/pic_h/19196.jpg");
             images.add("https://pbs.twimg.com/media/D7P_yLdX4AAvJWO?format=jpg&name=large");
@@ -52,8 +50,6 @@ class _WallpapersWidgetState extends State<WallpapersWidget> {
             images.add("https://images.hdqwalls.com/wallpapers/2019-cyberpunk-2077-4k-8t.jpg");
             images.add("https://images.alphacoders.com/102/thumb-1920-1021182.jpg");
             images.add("https://assets.vg247.com/current//2020/03/cyberpunk_wallpaper_3.jpg");
-            images.add("https://wallpapershome.com/images/wallpapers/cyberpunk-2077-1440x2560-e3-2019-screenshot-4k-21691.jpg");
-            images.add("https://assets1.ignimgs.com/2019/08/27/youre-mine-en-1566871767338.jpg");
             images.add("https://www.renderhub.com/renderhub/cyberpunk-2077-3d-wallpaper/cyberpunk-2077-3d-wallpaper_1920x1080.jpg");
             images.add("https://www.myplaystationwallpapers.net/1920x1080/cyberpunk-2077-pursuit.jpg");
             images.add("https://www.pxwall.com/wp-content/uploads/2018/08/cyberpunk-2077-4k-ultra-hd.jpg");
